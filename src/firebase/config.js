7641994aaa79195c1650433326ff6e000a364cf7
Nguyen Ngoc Minh Tue <nguyenngocminhtue510@gmail.com>
2024-10-12 09:45:57 +0700

    implement object

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,28 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAplzWPPWGl2YapmKUqhLqKkgqaBnIuOOM",
-//   authDomain: "chat-5b24d.firebaseapp.com",
-//   projectId: "chat-5b24d",
-//   storageBucket: "chat-5b24d.appspot.com",
-//   messagingSenderId: "249718293571",
-//   appId: "1:249718293571:web:396e6a1814e67244bdc0f6",
-//   measurementId: "G-47RKTRZM7R",
+//   apiKey: "AIzaSyAm75PsdFAgixGVqx-OU86rK0MerJMtSOo",
+//   authDomain: "fun-chat-475b7.firebaseapp.com",
+//   projectId: "fun-chat-475b7",
+//   storageBucket: "fun-chat-475b7.appspot.com",
+//   messagingSenderId: "259524468985",
+//   appId: "1:259524468985:web:c95663bd57ba6337744228",
+//   measurementId: "G-H78J7RRT18",
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAm75PsdFAgixGVqx-OU86rK0MerJMtSOo",
-  authDomain: "fun-chat-475b7.firebaseapp.com",
-  projectId: "fun-chat-475b7",
-  storageBucket: "fun-chat-475b7.appspot.com",
-  messagingSenderId: "259524468985",
-  appId: "1:259524468985:web:c95663bd57ba6337744228",
-  measurementId: "G-H78J7RRT18",
+  apiKey: "AIzaSyCjBxdNWOhdcFiqCjxIdNJ9aupbZ8XBOHk",
+  authDomain: "home-80235.firebaseapp.com",
+  projectId: "home-80235",
+  storageBucket: "home-80235.appspot.com",
+  messagingSenderId: "297283639098",
+  appId: "1:297283639098:web:b497c147d2cb491c7f6203",
+  measurementId: "G-XHH6L66YSM",
 };
 
 // Initialize Firebase
